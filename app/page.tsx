8173c660a14a4fbf6e226da3ebde5028c952d660
Viktor Birgisson <viktor.birgisson@gmail.com>
2024-07-19 17:23:31 +0000

    adding feature to fetch information about logged in user

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,25 @@
-import { Button } from "@/components/ui/button"
-import { validateRequest } from "@/lib/lucia"
-import { redirect } from "next/navigation"
-import { signOut } from "@/actions/auth.actions"
+import { Button } from "@/components/ui/button";
+import { validateRequest } from "@/lib/lucia";
+import { redirect } from "next/navigation";
+import { signOut } from "@/actions/auth.actions";
+import { lucia } from "@/lib/lucia";
 
 export default async function Home() {
-  const { user } = await validateRequest()
+  const { user, session } = await validateRequest();
 
   if (!user) {
-    return redirect("/sign-in")
+    return redirect("/sign-in");
   }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <p>Protected route</p>
-      <p>{JSON.stringify(user)}</p>
+      <p>
+        {JSON.stringify(user)} <br /> {JSON.stringify(session)}
+      </p>
       <form action={signOut}>
         <Button type="submit">Sign out</Button>
       </form>
     </main>
-  )
+  );
 }
